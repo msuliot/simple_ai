7c6766f8e44c52b4590a27301ca1cdae073856eb
Suliot <suliot@msn.com>
2023-05-22 15:32:02 -0700

    remove keys

--- data_import.py
+++ data_import.py
@@ -1,7 +1,5 @@
 # ------ > Run the command below in the terminal, use -- printenv -- to validate OPENAI_API_KEY
-# export OPENAI_API_KEY="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"
-
-# python3 -m pip freeze
+# export OPENAI_API_KEY="your_key_from_openai"
 
 from pathlib import Path
 from llama_index import download_loader
